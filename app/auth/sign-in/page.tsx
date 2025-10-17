a55c12669217248985b56e225f82dff34ca35a56
Cristhian Martinez <cristhian.martinezlara@gmail.com>
2025-10-17 17:03:21 -0600

    fix: Adjust margin and width for Sign In button and header section

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -7,7 +7,7 @@ export const SignIn = () => {
     <div className='mx-auto px-4 sm:px-6 lg:px-8 py-12'>
       <div className='min-h-[calc(100vh-250px)] flex flex-col justify-center items-center'>
         {/* Header Section */}
-        <div className='text-center mb-12'>
+        <div className='text-center mb-8'>
           <h1 className='text-5xl font-bold tracking-tight text-white mb-4'>Welcome!</h1>
           <p className='text-2lg text-subtitle mx-auto font-light'>
             Sign In to access your saved palettes and start sharing with the community.
@@ -17,7 +17,7 @@ export const SignIn = () => {
         {/* Sign In Section */}
         <div className='space-y-4'>
           <button
-            className='flex flex-row justify-center items-center min-w-xl bg-primary p-4 gap-x-2 rounded-2xl text-white font-bold hover:bg-primary/90 cursor-pointer'
+            className='flex flex-row justify-center items-center w-96 bg-primary p-4 gap-x-2 rounded-2xl text-white font-bold hover:bg-primary/90 cursor-pointer'
             onClick={() => console.log('sign in with google...')}
             type='button'
           >
